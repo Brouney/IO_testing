49a7a22ae6ab77af6d745a1eb1f4fa641b09233d
Brouney <piotreg98@gmail.com>
2020-05-15 19:30:36 +0200

    zmiany kosmetyczne oraz delikatne funkcjonowanie

--- TESTS/test_POST/post.py
+++ TESTS/test_POST/post.py
@@ -4,18 +4,18 @@ import requests
 class testPUT(unittest.TestCase):
     def testPostTournament(self):
         load = {'start_date':'01-01-2020', 'name': 'tournament1', 'city': 'krakow', 'location': 'ulica Pokątna'}
-        r = requests.post('http://77.55.192.26:2137/api/tournament', data = load)
+        r = requests.post('http://77.55.192.26:2137/api/tournament', json = load)
         self.assertEqual(r.status_code,200)
 
 
     def testPostAPhaseToATournament(self):
         load = {'name': 'nazwa', 'structure': {}}
-        r = requests.post('http://77.55.192.26:2137/api/tournament/3/phase', data = load)
+        r = requests.post('http://77.55.192.26:2137/api/tournament/3/phase', json = load)
         self.assertEqual(r.status_code,200)
 
     def testPostDebateToPhase(self):
-        load = {'d_time': 'time', 'd_date': 'date', 'location': 'varchar', 'team_1': 1, 'team_2': 2}
-        r = requests.post('http://77.55.192.26:2137/api/tournament/3/phase/7/debate',data = load)
+        load = {'d_time': '23:00:00.000', 'd_date': '2022-03-03', 'location': 'pod cerkwia'}
+        r = requests.post('http://77.55.192.26:2137/api/tournament/3/phase/8/debate',json = load)
         self.assertEqual(r.status_code,200)
 if __name__ == '__main__':
     
